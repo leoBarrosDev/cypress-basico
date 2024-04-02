870735f9469e5fc3bbb21797169c03a0c0edc87d
Leandro Reis <leohbr@gmail.com>
2024-04-02 17:49:20 -0300

    add valids informations test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,6 +1,81 @@
-// CAC-TAT.spec.js created with Cypress
-//
-// Start writing your Cypress tests below!
-// If you're unfamiliar with how Cypress works,
-// check out the link below and learn how to write your first test:
-// https://on.cypress.io/writing-first-test
+/// <reference types="Cypress" />
+
+describe('Central de atendimento ao cliente TAT', () => {
+
+    beforeEach(() => {
+        cy.visit('./src/index.html')
+    })
+    it('Verifica o título da aplicação', () => {
+        cy.title()
+            .should('eq', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('Preenche corretamente todos os campos do formulário', () => {
+        cy.get('#firstName')
+            .should('be.visible')
+            .clear()
+            .type('Leandro')
+
+        cy.get('#lastName')
+            .should('be.visible')
+            .clear()
+            .type('Reis')
+
+        cy.get('#email')
+            .should('be.visible')
+            .clear()
+            .type('leohbr@gmail.com')
+
+        cy.get('#phone')
+            .should('be.visible')
+            .clear()
+            .type('11999999999')
+
+        cy.get('#product')
+            .should('be.visible')
+            .select('Mentoria')
+
+        cy.get('#support-type')
+            .find('input[type="radio"]')
+            .then(radioButtons => {
+                // cy.wrap(radioButtons)
+                //     .first() 
+                //     .check() 
+                //     .should('be.checked'); 
+
+                cy.wrap(radioButtons)
+                    .eq(1)
+                    .check()
+                    .should('be.checked');
+
+                // cy.wrap(radioButtons)
+                //     .last() 
+                //     .check() 
+                //     .should('be.checked'); 
+
+            });
+        cy.get('#check')
+            .find('input[type="checkbox"]')
+            .each(checkbox => {
+                cy.wrap(checkbox).check().should('be.checked');
+            });
+
+        cy.get('#open-text-area')
+            .should('be.visible')
+            .type('Mensagem de teste');
+
+        cy.get('.button').then(buttons => {
+            buttons.each((button) => {
+                if (button.textContent === 'Enviar' && button.type === 'submit') {
+                    cy.wrap(button).click();
+                }
+            });
+        });
+    })
+
+
+
+    it('', () => {
+
+    })
+})
