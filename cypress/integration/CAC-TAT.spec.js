3a79f2f4c22aed7d248ae405726feeca7d08d149
Leandro Reis <leohbr@gmail.com>
2024-04-02 22:09:46 -0300

    add invalid email test

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -63,26 +63,81 @@ describe('Central de atendimento ao cliente TAT', () => {
         cy.get('#open-text-area')
             .should('be.visible')
             .clear()
-            .type('Mensagem de teste');
+            .type('Essa mensagem será digitada instantaneamente, delay com o valor igual a 70.', { delay: 0 });
 
-        cy.get('.button').then(buttons => {
-            buttons.each((button) => {
-                if (button.textContent === 'Enviar' && button.type === 'submit') {
-                    cy.wrap(button).click()
-                }
-                cy.get('span.success')
-                    .should('exist')
-                    .and('contain', 'Mensagem enviada com sucesso.');
+        cy.get('.button[type="submit"]')
+            .should('be.visible')
+            .should('be.enabled')
+            .and('contain', 'Enviar')
+            .click()
+        cy.get('span.success')
+            .should('exist')
+            .and('contain', 'Mensagem enviada com sucesso.');
+    })
+
+    it('Preenchendo e-mail inválido', () => {
+        cy.get('#firstName')
+            .should('be.visible')
+            .clear()
+            .type('Leandro')
+
+        cy.get('#lastName')
+            .should('be.visible')
+            .clear()
+            .type('Reis')
+
+        cy.get('#email')
+            .should('be.visible')
+            .clear()
+            .type('leohbr@gmail')
+
+        cy.get('#phone')
+            .should('be.visible')
+            .clear()
+            .type('11999999999')
+
+        cy.get('#product')
+            .should('be.visible')
+            .select('Mentoria')
+
+        cy.get('#support-type')
+            .find('input[type="radio"]')
+            .then(radioButtons => {
+                // cy.wrap(radioButtons)
+                //     .first() 
+                //     .check() 
+                //     .should('be.checked'); 
+
+                cy.wrap(radioButtons)
+                    .eq(1)
+                    .check()
+                    .should('be.checked');
+
+                // cy.wrap(radioButtons)
+                //     .last() 
+                //     .check() 
+                //     .should('be.checked'); 
+
             });
-        });
+        cy.get('#check')
+            .find('input[type="checkbox"]')
+            .each(checkbox => {
+                cy.wrap(checkbox).check().should('be.checked');
+            });
 
+        cy.get('#open-text-area')
+            .should('be.visible')
+            .clear()
+            .type('Essa mensagem será digitada instantaneamente, delay com o valor igual a 70.', { delay: 0 });
 
-
+        cy.get('.button[type="submit"]')
+            .should('be.visible')
+            .should('be.enabled')
+            .and('contain', 'Enviar')
+            .click()
+        cy.get('span.error')
+            .should('exist')
+            .and('contain', 'Valide os campos obrigatórios!');
     })
 
-
-
-    it('', () => {
-
-    })
-})
+});
